7678c71b7810d9b2bdd48786e6e6db074231f81a
YxyVarible <78844955+YxyVarible@users.noreply.github.com>
2021-02-10 16:50:52 +0800

    完全背包

---
+++ CompleteKnapsack.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int m,n,w[40],c[40],f[210];
+int main() {
+        cin >> m >> n;
+        for (int i = 1;i <= n;i ++)
+                cin >> w[i] >> c[i];
+        for (int i = 1;i <= n;i ++)
+                for (int j = w[i];j <= m;j ++)
+                        f[j] = max(f[j],f[j - w[i]] + c[i]);
+        cout << f[m] << endl;
+        return 0;
+}
